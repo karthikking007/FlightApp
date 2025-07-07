8f834b3ae1eae5c3e909b725b75a4ed5eaf537e2
karthikking007 <karthikbm910@gmail.com>
2025-07-07 16:10:46 +0530

    ojsdf

--- src/main/java/com/shanInfotech/FlightjdbcEx/App.java
+++ src/main/java/com/shanInfotech/FlightjdbcEx/App.java
@@ -6,8 +6,8 @@ import com.shanInfotech.FlightjdbcEx.dao.FlightDetailsDao;
 public class App {
     public static void main(String[] args) throws Exception {
       FlightDetailsDao fd=new FlightDetailsDao();
-      FlightDetails fde=new FlightDetails();
-      fd.addFlight(new FlightDetails());
+     // FlightDetails fde=new FlightDetails();
+     // fd.addFlight(new FlightDetails());
       fd.getFlight();
     }
 }
